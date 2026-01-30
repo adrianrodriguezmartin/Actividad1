d47b953f537096767ef7673043e9b4c3c5baf7c2
Adrián <Adrin@DESKTOP-I6I0KTV>
2026-01-30 10:56:28 +0100

    Primer commit del proyecto

---
+++ UD4_Actividad1/src/calculadora/Calculadora.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+public class Calculadora {
+	
+	 public int sumar(int a, int b) { 
+	        return a + b; 
+	    } 
+	 
+	    public int restar(int a, int b) { 
+	        return a - b; 
+	    }
+
+}
